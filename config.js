d558ea700e913857c5db0d28a01bb4a298535cf7
phatphan072 <21520389@gm.uit.edu.vn>
2021-11-29 20:18:57 +0700

    first commit

---
+++ config.js
@@ -0,0 +1,18 @@
+// 1. Chỉnh sửa các dòng chữ theo mục đích của bạn
+const CONFIG = {
+    introTitle: 'Hiiii Vi Hương!!!!',
+    introDesc: `Tôi có điều này muốn hỏi Em, Em nhớ phải trả lời thật lòng nha`,
+    btnIntro: 'OK',
+    title: 'Tôi thích em không biết em có thích tôi không? 😙',
+    desc: 'Em không trả lời hoặc thoát ra, tức là Em thích Tôi rồi đó nha :> ',
+    btnYes: 'Có, em thật sự thích anh 💖💖💖',
+    btnNo: '💔 Anh mơ à ! 💔 ',
+    question:'Nói cho Tôi biết đi vì sao Em thích tôi 😊😊',
+    reasonPlaceholder: 'Lý do',
+    btnReply: 'Gởi ! 💌💌',
+    reply: 'Tại vì anh dễ thương đó Ahiiiii 😊😊',
+    mess: 'Tôi biết mà, Tôi cũng thích em lắm 🥰. Love you 3000 😘😘',
+    messDesc: '💬💬 Nhắn tin cho Anh nha. 💬💬',
+    btnAccept: 'Ok lun 💖',
+    messLink: 'https://m.me/phatphan072.uit'
+}
